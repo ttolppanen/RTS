443bb48075f8fd65a0edfd9f95bcb44871047766
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2019-01-28 22:12:01 +0200

    Paremmat nimet

--- Assets/Scripts/UIChooser.cs
+++ Assets/Scripts/UIChooser.cs
@@ -41,7 +41,7 @@ public class UIChooser : MonoBehaviour
 
     public void stopUnits()
     {
-        List<GameObject> units = UnitControll.ins.giveChosenUnits();
+        List<GameObject> units = MouseControl.ins.giveChosenUnits();
         print("Click");
 
         foreach(GameObject unit in units)
