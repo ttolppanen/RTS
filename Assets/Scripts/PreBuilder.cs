2b4a331bbd0d1f2195789e4cc6114f087a6c8e56
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2019-01-28 18:12:56 +0200

    PreBuilderi
    Tulee näkyviin vihreänä tai punaisena pystyykö rakennusta rakentamaan johonkin paikkaan

---
+++ Assets/Scripts/PreBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreBuilder : MonoBehaviour
+{
+    public GameObject buildingToSpawn;
+    Vector2Int size;
+    SpriteRenderer sr;
+    Material bwMaterial; //BlackAndWhite material
+    Color canBuildColor = new Color(1, 1.5f, 1);
+    Color cannotBuildColor = new Color(1.5f, 1, 1);
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        bwMaterial = sr.material;
+        Sprite buildingSprite = buildingToSpawn.GetComponent<SpriteRenderer>().sprite;
+        sr.sprite = buildingSprite;
+        size = buildingToSpawn.GetComponent<BuildingStatus>().size;
+    }
+
+    private void Update()
+    {
+        Vector2Int point = UsefullFunctions.GetMousePosCoordinated();
+        transform.position = (Vector2)point;
+        if (Map.ins.CanBeBuilt(point, size))
+        {
+            bwMaterial.SetColor("_Color", canBuildColor);
+        }
+        else
+        {
+            bwMaterial.SetColor("_Color", cannotBuildColor);
+        }
+    }
+}
---
+++ Assets/Scripts/TestTestTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTestTest : MonoBehaviour
+{
+
+    public GameObject BuildingToInstantiate;
+    public GameObject preBuilder;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            Vector2 pos = UsefullFunctions.GetMousePosCoordinated();
+            GameObject ins = Instantiate(preBuilder, pos, Quaternion.identity);
+            PreBuilder script = ins.GetComponent<PreBuilder>();
+            script.buildingToSpawn = BuildingToInstantiate;
+        }       
+    }
+}
