6cb6fda9d95de0e65982aa21ef3e3e4737075ba6
Oskari <oskari266@hotmail.com>
2019-01-30 19:13:14 +0200

    Building UI

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    public enum BuildingTypes{WoodsPile};
+    public GameObject preBuilder;
+    public List<GameObject> buildings;
+    public void makeBuilding(BuildingTypes building)
+    {
+        switch(building)
+        {
+            case BuildingTypes.WoodsPile:
+                GameObject o = Instantiate(preBuilder);
+                o.GetComponent<PreBuilder>().buildingToSpawn = buildings[0];
+            break;
+        }
+
+    }
+
+    public void buildWoodPile()
+    {
+        makeBuilding(BuildingTypes.WoodsPile);
+    }
+}
