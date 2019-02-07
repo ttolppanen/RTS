e0cbca49bec738ee1d5de484bfcd950405a0812e
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2019-02-07 15:18:26 +0200

    Korjattu
    Resourssi struckti lisätty...

--- Assets/Scripts/Building/BuildingStatus.cs
+++ Assets/Scripts/Building/BuildingStatus.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 public class BuildingStatus : MonoBehaviour
 {
     public Vector2Int size;
-    public int[] resourceCosts = new int[2] { 0, 0}; //Puu, kivi
+    public Resource[] resourceCosts = new Resource[3];
+    public int woodCost;
+    public int stoneCost;
+    public int soulCost;
+
+    private void Awake()
+    {
+        resourceCosts = new Resource[]
+        {
+            new Resource(ResourceTypes.wood, woodCost),
+            new Resource(ResourceTypes.stone, stoneCost),
+            new Resource(ResourceTypes.soul, soulCost)
+        };
+    }
 }
